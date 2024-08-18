d76c44b025c28cc13769f3e82bbc2868d8380c79
S-Devisri01 <157806034+S-Devisri01@users.noreply.github.com>
2024-08-18 17:27:30 +0530

    Create useColorTheme.js

---
+++ useColorTheme.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { useColorMode } from '@chakra-ui/react';
+
+const useColorTheme = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const [isDark, setIsDark] = useState(colorMode === 'dark');
+
+  const toggleTheme = () => {
+    toggleColorMode();
+    setIsDark(!isDark);
+  };
+
+  return [isDark, toggleTheme];
+};
+
+export default useColorTheme;
